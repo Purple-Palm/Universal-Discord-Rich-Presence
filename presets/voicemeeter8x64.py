3f2f984e51c8221583cfb8d1d3a26968928b0679
Cactus <88003472+Purple-Palm@users.noreply.github.com>
2024-07-17 02:13:24 -0700

    Add files via upload

---
+++ presets/voicemeeter8x64.py
@@ -0,0 +1,42 @@
+import time
+import os
+import win32gui
+import win32process
+import psutil
+from pypresence import Presence
+
+def get_active_window_executable():
+    window = win32gui.GetForegroundWindow()
+    _, pid = win32process.GetWindowThreadProcessId(window)
+    try:
+        process = psutil.Process(pid)
+        return process.name()
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        return None
+
+def main():
+    client_id = '1262944099531755551'
+    RPC = Presence(client_id)
+    RPC.connect()
+
+    print("RPC connected, updating presence for voicemeeter8x64...")
+
+    start_time = time.time()
+    while True:
+        if get_active_window_executable() != "voicemeeter8x64.exe":
+            print("Voicemeeter8x64 is no longer in focus. Restarting main.py...")
+            RPC.clear()  # Clear the presence
+            RPC.close()  # Close the RPC connection
+            os.system("python main.py")
+            break
+
+        RPC.update(
+            state="Setting up some sound in voicemeeter",
+#            details="",
+            large_image="1",
+            start=start_time
+        )
+        time.sleep(15)  # Update every 15 seconds
+
+if __name__ == "__main__":
+    main()
