f0126661e4624b268f51e4cdc2d659a595171844
Cactus <88003472+Purple-Palm@users.noreply.github.com>
2024-07-17 02:16:01 -0700

    Add files via upload

---
+++ presets/valorant/VALORANT-Win64-Shipping.py
@@ -0,0 +1,15 @@
+import time
+import os
+import win32gui
+import win32process
+import psutil
+from pypresence import Presence
+import asyncio
+
+def main():
+    print("1 Minute delay before swapping RP for optimization.")
+    os.system("python presets/valorant/main.py")
+
+
+if __name__ == "__main__":
+    main()
---
+++ presets/valorant/version.py
@@ -0,0 +1,22 @@
+VSVersionInfo(
+    ffi=FixedFileInfo(
+        filevers=(3, 0, 0, 0),
+        prodvers=(3, 0, 0, 0),
+        mask=0x3f,
+        flags=0x0,
+        OS=0x4,
+        fileType=0x1,
+        subtype=0x0,
+        date=(0, 0)),
+    kids=[
+        StringFileInfo([StringTable(
+            u'040904B0',
+            [StringStruct(u'CompanyName', u'Colin Hartigan'),
+             StringStruct(u'FileDescription', u'VALORANT rich presence client'),
+             StringStruct(u'FileVersion', u'3.0'),
+             StringStruct(u'InternalName', u'rpcclient'),
+             StringStruct(u'LegalCopyright', u'Colin Hartigan'),
+             StringStruct(u'OriginalFilename', u'valorant-rpc.exe'),
+             StringStruct(u'ProductName', u'VALORANT-rpc'),
+             StringStruct(u'ProductVersion', u'3.1.0')])]),
+        VarFileInfo([VarStruct(u'Translation', [1033, 1200])])])
