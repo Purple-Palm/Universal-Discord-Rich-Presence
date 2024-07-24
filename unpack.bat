ac2f632aeaf4d3a3aeaeedc102c66ce537199498
Cactus <88003472+Purple-Palm@users.noreply.github.com>
2024-07-23 21:47:41 -0700

    Update unpack.bat

--- unpack.bat
+++ unpack.bat
@@ -1,19 +1,11 @@
 @echo off
 setlocal EnableDelayedExpansion
-
-REM Change to the "packed" directory
 cd presets\valorant\src\content\packed
-
-REM Loop through all files in the "packed" directory
 for %%f in (*) do (
-    REM Move the file to the parent directory
     move "%%f" ..\..\..\..\..
-
     REM Wait for 1 second
     timeout /t 1 /nobreak >nul
 )
-
-REM Start setup.bat after all files have been moved
 echo Please wait..
 cd ..
 cd ..
@@ -21,5 +13,4 @@ cd ..
 cd ..
 cd ..
 call setup.bat
-
 endlocal
