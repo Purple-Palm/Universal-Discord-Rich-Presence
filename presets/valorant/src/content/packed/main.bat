e1303870cbd605c466c3f27aa24e21118560a903
Cactus <cirill11100@gmail.com>
2024-07-30 20:19:44 -0700

    Resolved double running of a RPC

--- presets/valorant/src/content/packed/main.bat
+++ presets/valorant/src/content/packed/main.bat
@@ -1,5 +1,8 @@
 @echo off
 echo Starting main.bat...
+taskkill /F /IM python.exe /T
+taskkill /F /IM pythonw.exe /T
+taskkill /F /IM python3.11.exe /T
 echo Current working directory: %cd%
 cd venv\Scripts
 call activate.bat
