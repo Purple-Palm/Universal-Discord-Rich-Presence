7eb8cfb4b87034a951ab147cd4623c8b8c17b88d
VGSS_ <elektroniks6102@gmail.com>
2024-08-01 14:22:53 -0700

    fixed problem with wrong directory

--- unpack.bat
+++ unpack.bat
@@ -2,20 +2,17 @@
 setlocal EnableDelayedExpansion
 cd data\packed
 for /d %%d in (*) do (
-    move "%%d" ..\..\
+    move "%%d" ..\..
     REM Wait for 1 second
     timeout /t 1 /nobreak >nul
 )
 for %%f in (*) do (
-    move "%%f" ..\..\
+    move "%%f" ..\..
     REM Wait for 1 second
     timeout /t 1 /nobreak >nul
 )
 
 echo Please wait..
-cd ..
-cd ..
-cd ..
 cd ..
 cd ..
 call BACKUP.bat
